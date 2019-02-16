638c7bf5eadb33361cd8fc15ac721023be24c8f2
Ricardo <ricardocavalcantesilva@gmail.com>
2019-02-16 16:35:07 -0200

    Configurando Banco de dado

--- CasaDoCodigo/ApplicationContext.cs
+++ CasaDoCodigo/ApplicationContext.cs
@@ -19,6 +19,20 @@ namespace CasaDoCodigo
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Produto>().HasKey(p => p.Id);
+
+            modelBuilder.Entity<Pedido>().HasKey(p => p.Id);
+            modelBuilder.Entity<Pedido>().HasMany(p => p.Itens).WithOne(p => p.Pedido);
+            modelBuilder.Entity<Pedido>().HasOne(p => p.Cadastro).WithOne(p => p.Pedido).IsRequired();
+
+            modelBuilder.Entity<ItemPedido>().HasKey(p => p.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(p => p.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(p => p.Produto);
+
+
+            modelBuilder.Entity<Cadastro>().HasKey(p => p.Id);
+            modelBuilder.Entity<Cadastro>().HasOne(p => p.Pedido);
+
+
         }
     }
 }
