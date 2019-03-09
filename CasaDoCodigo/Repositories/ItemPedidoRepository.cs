6ac69aef6f018ef6e20d45bbdbf1c94497d7e1a6
Ricardo <ricardocavalcantesilva@gmail.com>
2019-03-09 07:21:20 -0300

    Alterando a quantidade dos itens

--- CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -6,12 +6,30 @@ using System.Threading.Tasks;
 
 namespace CasaDoCodigo.Repositories
 {
-    public interface IItemPedidoRepository { }
+    public interface IItemPedidoRepository
+    {
+        void UpdateQuantidade(ItemPedido itemPedido);
+    }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext context) : base(context)
         {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+            var itemPedidoDB =
+            dbSet
+                .Where(ip => ip.Id == itemPedido.Id)
+                .SingleOrDefault();
+
+            if (itemPedidoDB != null)
+            {
+                itemPedidoDB.AtualizaQuantidade(itemPedido.Quantidade);
+
+                context.SaveChanges();
+            }
         }
     }
 }
