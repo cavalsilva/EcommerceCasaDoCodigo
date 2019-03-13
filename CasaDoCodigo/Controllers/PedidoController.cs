c9ea6ae9cba09cc122d76dee4d8e4417ce3229e1
Ricardo <ricardocavalcantesilva@gmail.com>
2019-03-13 07:03:33 -0300

    Validando e enviando formulário de cadastro

--- CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/Controllers/PedidoController.cs
@@ -25,7 +25,14 @@ namespace CasaDoCodigo.Controllers
 
         public IActionResult Cadastro()
         {
-            return View();
+            var pedido = pedidoRepository.GetPedido();
+
+            if (pedido == null)
+            {
+                return RedirectToAction("Carrossel");
+            }
+
+            return View(pedido.Cadastro);
         }
 
         public IActionResult Carrinho(string codigo)
@@ -45,10 +52,16 @@ namespace CasaDoCodigo.Controllers
             return View(produtoRepository.GetProdutos());
         }
 
-        public IActionResult Resumo()
+        [HttpPost]
+        public IActionResult Resumo(Cadastro cadastro)
         {
-            Pedido pedido = pedidoRepository.GetPedido();
-            return View(pedido);
+            if (ModelState.IsValid)
+            {
+                return View(pedidoRepository.UpdateCadastro(cadastro));
+            }
+
+            return RedirectToAction("Cadastro");
+            
         }
 
         [HttpPost]
